6a7272ac6ce99a1956e24b8b046c0517a908de94
Pieter Erzeel <pieter.erzeel@gmail.com>
2018-03-09 23:56:01 +0100

    finally got the binding right 0_o

--- DMWorkshop.Web/ClientApp/components/Layout.tsx
+++ DMWorkshop.Web/ClientApp/components/Layout.tsx
@@ -9,31 +9,30 @@ import * as Encounters from '../store/Encounters';
 export default class Layout extends React.Component<{}, {}> {
     public render() {
         return (
-            <Sidebar.Pushable as={Segment}>
-                <Sidebar as={Menu} animation='push' width='thin' visible icon='labeled' vertical inverted>
-                    <Menu.Item as={NavLink} to="/creatures/">
-                        <Icon name="id card" />
-                        Creatures
+            <Grid>
+                <Grid.Column stretched width={1}>
+                    <Menu width='thin'  icon='labeled' vertical inverted>
+                        <Menu.Item as={NavLink} to="/creatures/">
+                            <Icon name="id card" />
+                            Creatures
 					</Menu.Item>
-                    <Menu.Item as={NavLink} to="/encounters/">
-                        <Icon name="sort content descending" />
-                        Encounters
+                        <Menu.Item as={NavLink} to="/encounters/">
+                            <Icon name="sort content descending" />
+                            Encounters
 					</Menu.Item>
-                    <Menu.Item as={NavLink} to="/party/">
-                        <Icon name="group" />
-                        Party
+                        <Menu.Item as={NavLink} to="/party/">
+                            <Icon name="group" />
+                            Party
 					</Menu.Item>
-                </Sidebar>
-                <Sidebar.Pusher>
-                    <Grid>
-                        <Grid.Column stretched>
-                            <Switch>
-                                {this.props.children}
-                            </Switch>
-                        </Grid.Column>
-                    </Grid>
-                </Sidebar.Pusher>
-            </Sidebar.Pushable>
+                    </Menu>
+                </Grid.Column>
+
+                <Grid.Column stretched width={15}>
+                        <Switch>
+                            {this.props.children}
+                        </Switch>
+                </Grid.Column>
+            </Grid>
         );
     }
 }
